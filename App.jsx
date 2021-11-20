c76b456c8c8c4413ef7d8f5153d5447da40369d1
r3v3r3seEntropy <93636350+r3v3r3seEntropy@users.noreply.github.com>
2021-11-20 15:53:30 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -162,7 +162,6 @@ const mobileHomepageMainData = {
     rectangle53: "/img/rectangle-53@2x.png",
     image10: "/img/image@2x.png",
     image11: "/img/image-7@2x.png",
-    recentPosts: "Recent Posts",
     supportUs: "Support Us",
     workWithUs: "Work with us",
     untitledDesign42: "/img/untitled-design-4--2@2x.png",
@@ -644,7 +643,7 @@ const homepageMain1Data = {
     phone3: "04/09/2021",
     rectangle53: "/img/rectangle-53@2x.png",
     readMore6: "READ MORE",
-    recentPosts: "Recent Posts",
+    
     untitledDesign41: "/img/untitled-design-4--1@2x.png",
     workWithUs: "Work with us",
     text105: <>If you believe in our approach and work and want to join the movement in any capacity, we would love to hear from you.<br /><br />Do drop us a line!</>,
